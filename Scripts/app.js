33954ab33d5282a3105a0fdd29368f1486348312
JMS-1 <dev-2@psimarron.net>
2015-01-01 23:52:00 +0100

    Started market selection

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,40 +1,97 @@
 var TheApplication;
 (function (TheApplication) {
+    var homeName = '#homePage';
+    var marketListName = '#marketList';
+    var marketDetailsName = '#marketDetail';
     var marketSelectionList;
+    var marketDetails;
+    var homePage;
+    // Set as soon as we start buying
+    var currentMarket = null;
     // Synchronized startup code
     $(function () {
-        marketSelectionList = new MarketSelectionList($('#marketList'));
+        marketSelectionList = new MarketSelectionList($(marketListName));
+        marketDetails = new MarketDetails($(marketDetailsName));
+        homePage = new HomePage($(homeName));
     });
+    // Market effective data implementation
     var Market = (function () {
-        function Market() {
+        function Market(fromStore) {
+            this.name = fromStore.name;
         }
+        Market.prototype.appendTo = function (items) {
+            var _this = this;
+            var choose = $('<a/>', { text: this.name, href: homeName });
+            var edit = $('<a/>', { href: marketDetailsName });
+            choose.on('click', function () { return currentMarket = _this; });
+            edit.on('click', function () { return Market.detailsScope = _this; });
+            items.append($('<li/>').append(choose).append(edit));
+        };
+        Market.compare = function (left, right) {
+            return left.name.localeCompare(right.name);
+        };
         return Market;
     })();
+    // The home page of it all
+    var HomePage = (function () {
+        function HomePage(list) {
+            var _this = this;
+            this.list = list;
+            list.on('pagebeforeshow', function () { return _this.onShow(); });
+            this.onShow();
+        }
+        HomePage.prototype.onShow = function () {
+            var headerText = this.list.find('[data-role=header] h1');
+            var market = currentMarket;
+            if (market == null)
+                headerText.text('Deine Einkaufsliste');
+            else
+                headerText.text('Einkaufen bei ' + market.name);
+        };
+        return HomePage;
+    })();
+    // The market selection list page
     var MarketSelectionList = (function () {
         function MarketSelectionList(list) {
             var _this = this;
             this.list = list;
             list.on('pagecreate', function () { return _this.onCreated(); });
         }
-        MarketSelectionList.prototype.add = function (market) {
-            $('<li>', { text: market.name }).appendTo(this.items);
-        };
         MarketSelectionList.prototype.load = function () {
             var _this = this;
-            this.markets = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
-            this.markets.sort(function (l, r) { return l.name.localeCompare(r.name); });
-            $.each(this.markets, function (i, market) { return _this.add(market); });
+            var storedMarkets = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
+            this.markets = $.map(storedMarkets, function (stored) { return new Market(stored); });
+            this.markets.sort(Market.compare);
+            $.each(this.markets, function (i, market) { return market.appendTo(_this.items); });
             this.items.listview('refresh');
         };
         MarketSelectionList.prototype.save = function () {
             localStorage[MarketSelectionList.storageKey] = JSON.stringify(this.markets);
         };
         MarketSelectionList.prototype.onCreated = function () {
+            this.list.find('#newMarket').on('click', function () { return Market.detailsScope = null; });
             this.items = this.list.find('[data-role=listview]');
             this.load();
         };
         MarketSelectionList.storageKey = 'MarketList';
         return MarketSelectionList;
+    })();
+    // The market details page
+    var MarketDetails = (function () {
+        function MarketDetails(list) {
+            var _this = this;
+            this.list = list;
+            list.on('pagebeforeshow', function () { return _this.onShow(); });
+        }
+        MarketDetails.prototype.onShow = function () {
+            var headerText = this.list.find('[data-role=header] h1');
+            var market = Market.detailsScope;
+            if (market == null)
+                headerText.text('Neuen Markt anlegen');
+            else
+                headerText.text('Marktdaten verändern');
+        };
+        return MarketDetails;
     })();
 })(TheApplication || (TheApplication = {}));
 //# sourceMappingURL=app.js.map
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,20 +1,88 @@
 ﻿
 module TheApplication {
+    var homeName = '#homePage';
+    var marketListName = '#marketList';
+    var marketDetailsName = '#marketDetail';
+
     var marketSelectionList: MarketSelectionList;
+    var marketDetails: MarketDetails;
+    var homePage: HomePage;
+
+    // Set as soon as we start buying
+    var currentMarket: IMarket = null;
 
     // Synchronized startup code
     $(() => {
-        marketSelectionList = new MarketSelectionList($('#marketList'));
+        marketSelectionList = new MarketSelectionList($(marketListName));
+        marketDetails = new MarketDetails($(marketDetailsName));
+        homePage = new HomePage($(homeName));
     });
 
-    class Market {
+    // Market raw data as serialized to store
+    interface IStoredMarket {
+        // Name of the market
         name: string;
     }
 
+    // Market effective data
+    interface IMarket extends IStoredMarket {
+        // Create list view row for market including selection and edit using split mode
+        appendTo(items: JQuery): void;
+    }
+
+    // Market effective data implementation
+    class Market implements IMarket {
+        // Current market for details form
+        static detailsScope: IMarket;
+
+        constructor(fromStore: IStoredMarket) {
+            this.name = fromStore.name;
+        }
+
+        name: string;
+
+        appendTo(items: JQuery): void {
+            var choose = $('<a/>', { text: this.name, href: homeName });
+            var edit = $('<a/>', { href: marketDetailsName });
+
+            choose.on('click', () => currentMarket = this);
+            edit.on('click', () => Market.detailsScope = this);
+
+            items
+                .append($('<li/>')
+                    .append(choose)
+                    .append(edit));
+        }
+
+        static compare(left: IStoredMarket, right: IStoredMarket): number {
+            return left.name.localeCompare(right.name);
+        }
+    }
+
+    // The home page of it all
+    class HomePage {
+        constructor(private list: JQuery) {
+            list.on('pagebeforeshow', () => this.onShow());
+
+            this.onShow();
+        }
+
+        private onShow(): void {
+            var headerText = this.list.find('[data-role=header] h1');
+            var market = currentMarket;
+
+            if (market == null)
+                headerText.text('Deine Einkaufsliste');
+            else
+                headerText.text('Einkaufen bei ' + market.name);
+        }
+    }
+
+    // The market selection list page
     class MarketSelectionList {
         private static storageKey = 'MarketList';
 
-        private markets: Market[];
+        private markets: IMarket[];
 
         private items: JQuery;
 
@@ -22,16 +90,13 @@ module TheApplication {
             list.on('pagecreate', () => this.onCreated());
         }
 
-        private add(market: Market): void {
-            $('<li>', { text: market.name }).appendTo(this.items);
-        }
-
         private load(): void {
-            this.markets = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
+            var storedMarkets: IStoredMarket[] = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
 
-            this.markets.sort((l, r) => l.name.localeCompare(r.name));
+            this.markets = $.map(storedMarkets, stored => new Market(stored));
+            this.markets.sort(Market.compare);
 
-            $.each(this.markets, (i, market) => this.add(market));
+            $.each(this.markets, (i, market) => market.appendTo(this.items));
 
             this.items.listview('refresh');
         }
@@ -41,8 +106,27 @@ module TheApplication {
         }
 
         private onCreated(): void {
+            this.list.find('#newMarket').on('click', () => Market.detailsScope = null);
+
             this.items = this.list.find('[data-role=listview]');
             this.load();
+        }
+    }
+
+    // The market details page
+    class MarketDetails {
+        constructor(private list: JQuery) {
+            list.on('pagebeforeshow', () => this.onShow());
+        }
+
+        private onShow(): void {
+            var headerText = this.list.find('[data-role=header] h1');
+            var market = Market.detailsScope;
+
+            if (market == null)
+                headerText.text('Neuen Markt anlegen');
+            else
+                headerText.text('Marktdaten verändern');
         }
     }
 }
