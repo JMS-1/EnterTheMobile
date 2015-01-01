ff308a7ec542e96ccc875d03625f25112152a32f
JMS-1 <dev-2@psimarron.net>
2015-01-01 14:40:57 +0100

    Started with market selection list to refresh skills

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,40 @@
+var TheApplication;
+(function (TheApplication) {
+    var marketSelectionList;
+    // Synchronized startup code
+    $(function () {
+        marketSelectionList = new MarketSelectionList($('#marketList'));
+    });
+    var Market = (function () {
+        function Market() {
+        }
+        return Market;
+    })();
+    var MarketSelectionList = (function () {
+        function MarketSelectionList(list) {
+            var _this = this;
+            this.list = list;
+            list.on('pagecreate', function () { return _this.onCreated(); });
+        }
+        MarketSelectionList.prototype.add = function (market) {
+            $('<li>', { text: market.name }).appendTo(this.items);
+        };
+        MarketSelectionList.prototype.load = function () {
+            var _this = this;
+            this.markets = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
+            this.markets.sort(function (l, r) { return l.name.localeCompare(r.name); });
+            $.each(this.markets, function (i, market) { return _this.add(market); });
+            this.items.listview('refresh');
+        };
+        MarketSelectionList.prototype.save = function () {
+            localStorage[MarketSelectionList.storageKey] = JSON.stringify(this.markets);
+        };
+        MarketSelectionList.prototype.onCreated = function () {
+            this.items = this.list.find('[data-role=listview]');
+            this.load();
+        };
+        MarketSelectionList.storageKey = 'MarketList';
+        return MarketSelectionList;
+    })();
+})(TheApplication || (TheApplication = {}));
 //# sourceMappingURL=app.js.map
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,1 +1,49 @@
 ﻿
+module TheApplication {
+    var marketSelectionList: MarketSelectionList;
+
+    // Synchronized startup code
+    $(() => {
+        marketSelectionList = new MarketSelectionList($('#marketList'));
+    });
+
+    class Market {
+        name: string;
+    }
+
+    class MarketSelectionList {
+        private static storageKey = 'MarketList';
+
+        private markets: Market[];
+
+        private items: JQuery;
+
+        constructor(private list: JQuery) {
+            list.on('pagecreate', () => this.onCreated());
+        }
+
+        private add(market: Market): void {
+            $('<li>', { text: market.name }).appendTo(this.items);
+        }
+
+        private load(): void {
+            this.markets = JSON.parse(localStorage[MarketSelectionList.storageKey] || null) || [];
+
+            this.markets.sort((l, r) => l.name.localeCompare(r.name));
+
+            $.each(this.markets, (i, market) => this.add(market));
+
+            this.items.listview('refresh');
+        }
+
+        private save(): void {
+            localStorage[MarketSelectionList.storageKey] = JSON.stringify(this.markets);
+        }
+
+        private onCreated(): void {
+            this.items = this.list.find('[data-role=listview]');
+
+            this.load();
+        }
+    }
+}
