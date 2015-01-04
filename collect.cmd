63dd383ac9bd4da1d3b8a792786d8c1357b72b84
JMS-1 <dev-2@psimarron.net>
2015-01-04 23:58:03 +0100

    Added poor man's publish

---
+++ collect.cmd
@@ -0,0 +1,21 @@
+mkdir publish
+xcopy /y index.html publish
+xcopy /y sync.php publish
+xcopy /y user.php publish
+
+mkdir publish\content
+xcopy /y Content\app.css publish\content
+xcopy /y Content\jquery.mobile-1.4.2.css publish\content
+
+mkdir publish\content\images
+xcopy /y Content\images\ajax-loader.gif publish\content\images
+
+mkdir publish\scripts
+xcopy /y Scripts\jquery-2.1.1.min.js publish\scripts
+xcopy /y Scripts\jquery.mobile-1.4.2.min.js publish\scripts
+xcopy /y Scripts\user.js publish\scripts
+xcopy /y Scripts\item.js publish\scripts
+xcopy /y Scripts\market.js publish\scripts
+xcopy /y Scripts\app.js publish\scripts
+
+pause
