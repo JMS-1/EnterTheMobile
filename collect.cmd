4531567b094bb0fac54d59daf7e47708ba0cc8bd
JMS-1 <dev-2@psimarron.net>
2015-01-10 13:34:34 +0100

    Update to lastest jQuery version (NuGet)

--- collect.cmd
+++ collect.cmd
@@ -7,14 +7,14 @@ xcopy /y cache.manifest publish
 
 mkdir publish\content
 xcopy /y Content\app.css publish\content
-xcopy /y Content\jquery.mobile-1.4.2.css publish\content
+xcopy /y Content\jquery.mobile-1.4.5.css publish\content
 
 mkdir publish\content\images
 xcopy /y Content\images\ajax-loader.gif publish\content\images
 
 mkdir publish\scripts
-xcopy /y Scripts\jquery-2.1.1.min.js publish\scripts
-xcopy /y Scripts\jquery.mobile-1.4.2.min.js publish\scripts
+xcopy /y Scripts\jquery-2.1.3.min.js publish\scripts
+xcopy /y Scripts\jquery.mobile-1.4.5.min.js publish\scripts
 xcopy /y Scripts\user.js publish\scripts
 xcopy /y Scripts\item.js publish\scripts
 xcopy /y Scripts\market.js publish\scripts
